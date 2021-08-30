cf5ed09d57f8c69a0c9933942553f111b4280fef
Andrei Li <andreiliphd@gmail.com>
2021-08-30 21:10:04 +0300

    Place API implemented.

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -2,6 +2,7 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
@@ -9,6 +10,7 @@ import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.content.res.Resources.NotFoundException
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.*
@@ -16,6 +18,7 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -38,6 +41,8 @@ import kotlinx.android.synthetic.main.fragment_select_location.*
 import org.koin.android.ext.android.inject
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.udacity.project4.locationreminders.savereminder.SaveReminderFragmentDirections
 
@@ -55,6 +60,21 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnM
     // The entry point to the Fused Location Provider.
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private var lastKnownLocation: Location? = null
+    private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAttributions: Array<List<*>?> = arrayOfNulls(0)
+    private var likelyPlaceLatLngs: Array<LatLng?> = arrayOfNulls(0)
+    private var cameraPosition: CameraPosition? = null
+    private val REQUEST_PERMISSIONS_REQUEST_CODE: Int = 34
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -67,7 +87,7 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnM
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
         // Construct a PlacesClient
-        Places.initialize(requireContext(), savedInstanceState?.getString("com.google.android.geo.API_KEY")!!)
+        Places.initialize(requireContext(), "AIzaSyAqBewn-aBdn5jc_Dd5aF8Gel8alLfZFe8")
         placesClient = Places.createClient(requireContext())
 
         // Construct a FusedLocationProviderClient.
@@ -130,6 +150,36 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnM
         } catch (e: NotFoundException) {
             Log.e("maps", "Can't find style. Error: ", e)
         }
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(59.935252086183695, 30.325599254316298)
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        mMap.setOnMarkerClickListener(this)
+//        mMap.setOnMapClickListener {
+//            Log.i("marker", it.toString())
+//            val marker = mMap.addMarker(MarkerOptions()
+//                .position(it)
+//                .draggable(true)
+//                .title("Set point of interest."))
+//            AlertDialog.Builder(requireContext())
+//                .setTitle("Add marker")
+//                .setMessage("Do you want to add marker to reminders?")
+//                .setPositiveButton("Yes") { _, _ ->
+//                    val action =
+//                        SelectLocationFragmentDirections
+//                            .actionSelectLocationFragmentToSaveReminderFragment(marker.position)
+//                    view?.findNavController()?.navigate(action)
+//                    Log.i("marker", "yes")
+//                }
+//                .setNegativeButton("No") { _, _ ->
+//                    Log.i("marker", "no")
+//                }
+//                .show()
+//        }
+
         this.mMap?.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
             // Return null here, so that getInfoContents() is called next.
             override fun getInfoWindow(arg0: Marker): View? {
@@ -140,7 +190,7 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnM
                 // Inflate the layouts for the info window, title and snippet.
                 val infoWindow = layoutInflater.inflate(
                     R.layout.custom_info_contents,
-                    childFragmentManager.findFragmentById(R.id.map) as ViewGroup,
+                    requireView().findViewById(R.id.map),
                     false
                 )
                 val title = infoWindow.findViewById<TextView>(R.id.titleInfo)
@@ -151,43 +201,278 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnM
             }
         })
 
-//        // Prompt the user for permission.
-//        getLocationPermission()
-//
-//        // Turn on the My Location layer and the related control on the map.
-//        updateLocationUI()
-//
-//        // Get the current location of the device and set the position of the map.
-//        getDeviceLocation()
+        // Prompt the user for permission.
+        getLocationPermission()
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(59.935252086183695, 30.325599254316298)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        mMap.setOnMarkerClickListener(this)
-        mMap.setOnMapClickListener {
-            Log.i("marker", it.toString())
-            val marker = mMap.addMarker(MarkerOptions()
-                .position(it)
-                .draggable(true)
-                .title("Set point of interest."))
-            AlertDialog.Builder(requireContext())
-                .setTitle("Add marker")
-                .setMessage("Do you want to add marker to reminders?")
-                .setPositiveButton("Yes") { _, _ ->
-                    val action =
-                        SelectLocationFragmentDirections
-                            .actionSelectLocationFragmentToSaveReminderFragment(marker.position)
-                    view?.findNavController()?.navigate(action)
-                    Log.i("marker", "yes")
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+    }
+
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        mMap?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
                 }
-                .setNegativeButton("No") { _, _ ->
-                    Log.i("marker", "no")
-                }
-                .show()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
         }
+    }
+
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private fun getLocationPermission() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+            requireActivity(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(
+                "WoosmapGeofencing",
+                "Displaying permission rationale to provide additional context."
+            )
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        } else {
+            Log.i("WoosmapGeofencing", "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        }
+
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+//        if (ContextCompat.checkSelfPermission(requireContext(),
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//            == PackageManager.PERMISSION_GRANTED) {
+//            locationPermissionGranted = true
+//        } else {
+//            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+//        }
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        locationPermissionGranted = false
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i("WoosmapGeofencing", "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("WoosmapGeofencing", "Permission granted, updates requested, starting location updates");
+                locationPermissionGranted = true
+            } else {
+                // Permission denied.
+                locationPermissionGranted = false
+            }
+        }
+
+
+//            when (requestCode) {
+//            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+//
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.isNotEmpty() &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    locationPermissionGranted = true
+//                }
+//            }
+        updateLocationUI()
+    }
+
+    /**
+     * Prompts the user to select the current place from a list of likely places, and shows the
+     * current place on the map - provided the user has granted location permission.
+     */
+    @SuppressLint("MissingPermission")
+    private fun showCurrentPlace() {
+        if (mMap == null) {
+            return
+        }
+        if (locationPermissionGranted) {
+            // Use fields to define the data types to return.
+            val placeFields = listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)
+
+            // Use the builder to create a FindCurrentPlaceRequest.
+            val request = FindCurrentPlaceRequest.newInstance(placeFields)
+
+            // Get the likely places - that is, the businesses and other points of interest that
+            // are the best match for the device's current location.
+            val placeResult = placesClient.findCurrentPlace(request)
+            placeResult.addOnCompleteListener { task ->
+                if (task.isSuccessful && task.result != null) {
+                    Log.i("place", placeResult.result.toString())
+                    val likelyPlaces = task.result
+
+                    // Set the count, handling cases where less than 5 entries are returned.
+                    val count = if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
+                        likelyPlaces.placeLikelihoods.size
+                    } else {
+                        M_MAX_ENTRIES
+                    }
+                    var i = 0
+                    likelyPlaceNames = arrayOfNulls(count)
+                    likelyPlaceAddresses = arrayOfNulls(count)
+                    likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
+                    likelyPlaceLatLngs = arrayOfNulls(count)
+                    for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
+                        // Build a list of likely places to show the user.
+                        likelyPlaceNames[i] = placeLikelihood.place.name
+                        likelyPlaceAddresses[i] = placeLikelihood.place.address
+                        likelyPlaceAttributions[i] = placeLikelihood.place.attributions
+                        likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
+                        i++
+                        if (i > count - 1) {
+                            break
+                        }
+                    }
+
+                    // Show a dialog offering the user the list of likely places, and add a
+                    // marker at the selected place.
+                    openPlacesDialog()
+                } else {
+                    Log.e(TAG, "Exception: %s", task.exception)
+                }
+            }
+        } else {
+            // The user has not granted permission.
+            Log.i(TAG, "The user did not grant location permission.")
+
+            // Add a default marker, because the user hasn't selected a place.
+            mMap?.addMarker(MarkerOptions()
+                .title("Place")
+                .position(defaultLocation)
+                .snippet("No places found, because location permission is disabled."))
+
+            // Prompt the user for permission.
+            getLocationPermission()
+        }
+    }
+
+    /**
+     * Displays a form allowing the user to select a place from a list of likely places.
+     */
+    private fun openPlacesDialog() {
+        // Ask the user to choose the place where they are now.
+        val listener = DialogInterface.OnClickListener { dialog, which -> // The "which" argument contains the position of the selected item.
+            val markerLatLng = likelyPlaceLatLngs[which]
+            var markerSnippet = likelyPlaceAddresses[which]
+            if (likelyPlaceAttributions[which] != null) {
+                markerSnippet = """
+                    $markerSnippet
+                    ${likelyPlaceAttributions[which]}
+                    """.trimIndent()
+            }
+
+            // Add a marker for the selected place, with an info window
+            // showing information about that place.
+            mMap?.addMarker(MarkerOptions()
+                .title(likelyPlaceNames[which])
+                .position(markerLatLng!!)
+                .snippet(markerSnippet))
+
+            // Position the map's camera at the location of the marker.
+            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng,
+                DEFAULT_ZOOM.toFloat()))
+        }
+
+        // Display the dialog.
+        AlertDialog.Builder(requireContext())
+            .setTitle("Please, pick a place.")
+            .setItems(likelyPlaceNames, listener)
+            .show()
+    }
+
+    /**
+     * Updates the map's UI settings based on whether the user has granted location permission.
+     */
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap?.isMyLocationEnabled = true
+                mMap?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                mMap?.isMyLocationEnabled = false
+                mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    companion object {
+        private val TAG = "map"
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+
+        // Used for selecting the current place.
+        private const val M_MAX_ENTRIES = 5
     }
 
     /** Called when the user clicks a marker.  */
