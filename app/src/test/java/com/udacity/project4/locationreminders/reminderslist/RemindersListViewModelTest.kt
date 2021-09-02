c961a76c388bd6be61d5dfcbfdd92b7c2e252565
Andrei Li <al@skytech.app>
2021-09-02 19:59:00 +0300

    Tests are not working.

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,59 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.rule.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private lateinit var fakeReminderDataSource: FakeDataSource
+    private lateinit var remindersViewModel: RemindersListViewModel
 
+    @get:Rule
+    var instantTaskRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun setupTestViewModel() {
+        fakeReminderDataSource = FakeDataSource()
+        remindersViewModel = RemindersListViewModel( ApplicationProvider.getApplicationContext(), fakeReminderDataSource)
+    }
+
+    @After
+    fun cancelTestViewModel() {
+        stopKoin()
+    }
+
+    @Test
+    fun getRemindersViewModel_checkShowLoadingValue_returnsTrueFalse() {
+        // When
+        mainCoroutineRule.pauseDispatcher()
+        remindersViewModel.loadReminders()
+
+        // Then
+        assertThat(remindersViewModel.showLoading.value, `is`(true))
+
+        // When
+        mainCoroutineRule.resumeDispatcher()
+
+        // Then
+        assertThat(remindersViewModel.showLoading.value, `is`(false))
+    }
 }
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,29 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.R
+import com.udacity.project4.locationreminders.rule.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
-
-
-    //TODO: provide testing to the SaveReminderView and its live data objects
-
+    private lateinit var fakeReminderDataSource: FakeDataSource
+    private lateinit var remindersViewModel: SaveReminderViewModel
 
 }
